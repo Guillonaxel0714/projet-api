50387b4ee8c319f4b5abe6956e2576eea4530546
Guillonaxel0714 <55197055+Guillonaxel0714@users.noreply.github.com>
2020-10-18 23:17:43 +0200

    Update User.php

--- User.php
+++ User.php
@@ -8,37 +8,4 @@ class User extends General{
 
     protected $table = __CLASS__;
 
-    /**
-     * Client connexion
-     *
-     * @param array $param
-     */
-    public function connexion($param)
-    {
-        $statement = ("SELECT * FROM user WHERE mail='". $param["mail"] ."'");
-        $user = $this->db->queryReturn($statement, true);
-        if (password_verify($param["password"], $user["password"])) {
-            $key = "demo";
-            $payload = array(
-                "exp" => time() * 1200,
-                "id" => $user["id"]
-            );
-            $token = JWT::encode($payload, $key);
-            $this->db->sendData("connexion ok", true, $token);
-        }
-    }
-
-    /**
-     * Save client in Db
-     *
-     * @param array $param
-     */
-    public function save($param){
-        $statement = "INSERT INTO $this->table (mail, password) 
-                        VALUES (:mail, :password)";
-
-        $param["password"] = password_hash($param["password"], PASSWORD_DEFAULT);
-
-        $this->db->prepare($statement, "save", $param);
-    }
 }
